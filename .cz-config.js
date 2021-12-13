183572436c926e63069ff00501a78f5528e88589
_lmmmmmm <931420654@qq.com>
2021-12-13 20:44:09 +0800

    [特性](配置文件): 增加对 Git 提交信息的规范检查
    配置了 commitlint & cz-customizable，可使用 npm run commit 生成规范的提交信息

---
+++ .cz-config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  types: [
+    { value: '特性', name: '特性:    一个新的特性' },
+    { value: '修复', name: '修复:    修复一个Bug' },
+    { value: '文档', name: '文档:    变更的只有文档' },
+    { value: '格式', name: '格式:    空格, 分号等格式修复' },
+    { value: '重构', name: '重构:    代码重构，注意和特性、修复区分开' },
+    { value: '性能', name: '性能:    提升性能' },
+    { value: '测试', name: '测试:    添加一个测试' },
+    { value: '工具', name: '工具:    开发工具变动(构建、脚手架工具等)' },
+    { value: '回滚', name: '回滚:    代码回退' }
+  ],
+  typePrefix: '[',
+  typeSuffix: ']',
+  scopes: [{ name: '配置文件' }],
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*
+  scopeOverrides: {
+    fix: [
+      {name: 'merge'},
+      {name: 'style'},
+      {name: 'e2eTest'},
+      {name: 'unitTest'}
+    ]
+  },
+  */
+  // override the messages, defaults are as follows
+  messages: {
+    type: '为确保提交遵循规范!\n选择你要提交的类型: ',
+    scope: '选择一个 scope (可选): ',
+    // used if allowCustomScopes is true
+    customScope: '请输入自定义的 scope: ',
+    subject: '填写简短的变更描述: \n',
+    body: '填写更加详细的变更描述（可选）。使用 "|" 换行: \n',
+    breaking: '列举非兼容性重大的变更（可选）: \n',
+    footer: '关联关闭的 issue(可选)，例如: #31, #34:\n',
+    confirmCommit: '确认提交？'
+  },
+  allowBreakingChanges: ['特性', '修复'],
+  allowCustomScopes: true,
+  subjectLimit: 100,
+  breaklineChar: '|'
+};
